f86c75ef7ab614eb4d14e2bf24d1267b49879e69
Neharika Jaie <neharikajaie89@gmail.com>
2025-07-21 13:29:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// const btn =document.querySelector("button")
+
+// btn.addEventListener("click", ()=>{
+//     let parent=btn.parentElement
+//     console.log(parent);
+
+//     parent.style.height="100px"
+//     parent.style.width="100px"
+//     parent.style.backgroundColor="green"
+
+// })
+
+// const list = document.querySelector(".list")
+
+// addEventListener("click",()=>{
+    // console.log(list.children);
+
+    // console.log(list.FirstelementChild);
+    // console.log(list.LastElementChild);
+    // console.log(list.FirstChild);
+    // console.log(list.LastChild);
+
+    // console.log(list.id);
+    // list.id ="list1"
+
+    // console.log(list.id);
+
+    // console.log(list.nextElementSibling);
+    // console.log(list.previousElementSibling);
+
+// })
+
+let box = document.querySelector(".box");
+
+box.addEventListener("Mouse over",()=>{
+    console.log("mouse aaya");
+})
+
+box.addEventListener("Mouseout",()=>{
+    console.log("mouse gaya");
+})
+
